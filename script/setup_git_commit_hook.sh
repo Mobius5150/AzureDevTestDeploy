907076df8ffe83ee1a210c4fdbc6723c1ef65097
Mike Blouin <mike@michaelblouin.ca>
2015-11-03 01:20:31 -0700

    Document git hook setup script.

--- script/setup_git_commit_hook.sh
+++ script/setup_git_commit_hook.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# This script configures the local git repo to automatically run jenkins tests after each commit.
+# The post-commit hook will not cause the commit to fail if jenkins cannot be reached (if the container is not running)
+
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 HOOK="${SCRIPTDIR}/../.git/hooks/post-commit"
